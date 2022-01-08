f8893a636b708ed76e1612865655d7fc9a756693
Mark Maksi <68314514+markmaksi74@users.noreply.github.com>
2022-01-08 18:11:28 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+//jshint esversion:6
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 8000
+}
+
+const express = require("express");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const {getDefaultList, getCustomList, postDefaultList, deleteItem} = require("./controller/lists.controller")
+
+const app = express();
+app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://mark:Dj3Hfhdi7jrhf5dvda34g@cluster0.z8zd9.mongodb.net/todoList');
+}
+
+app.get("/", getDefaultList);
+
+app.get("/:customListName", getCustomList);
+
+app.post("/", postDefaultList);
+
+app.post("/delete", deleteItem);
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(port, function() {
+  console.log("Server started on port" + port);
+});
